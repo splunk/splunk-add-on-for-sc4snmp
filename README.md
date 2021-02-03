7a012266b5dbb1077032147a3846895d408a8b21
Addon Factory template <addonfactory@splunk.com>
2021-02-03 23:47:53 +0000

    base

---
+++ README.md
@@ -0,0 +1,33 @@
+## Setup of developer env
+
+Note: Must install docker desktop, vscode or pycharm pro optional
+
+Note2: Appinspect requires libmagic verify this has been installed correctly each time a new workstation/vm is used https://dev.splunk.com/enterprise/docs/releaseapps/appinspect/splunkappinspectclitool/installappinspect
+
+```bash
+git clone git@github.com:splunk/<repo slug>.git
+cd <repo dir>
+git submodule update --init --recursive
+
+#setup python venv must be 3.7
+/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install https://download.splunk.com/misc/appinspect/splunk-appinspect-latest.tar.gz
+
+```
+
+
+## Test
+
+Using docker 
+
+```bash
+pytest
+```
+
+Using external Splunk instance with Eventgen and app pre-installed
+
+```bash
+pytest --splunk_type=external --splunk_host=something --splunk_user=foo --splunk_password=something
+```
---
+++ tests/entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+##
+## SPDX-FileCopyrightText: 2020 Splunk, Inc. <sales@splunk.com>
+## SPDX-License-Identifier: LicenseRef-Splunk-1-2020
+##
+##
+
+cd /home/circleci/work
+pip install pip==20.2
+if [ -f "${TEST_SET}/pytest-ci.ini" ]; then
+    cp -f ${TEST_SET}/pytest-ci.ini pytest.ini
+fi
+
+# Installing the lib folder inside requirements for unit test case stage
+if [ "${TEST_TYPE}" = "unit" ]
+then
+    [ "${IMAGE_TAG}" = "2.7.17" ] && FOLDER_NAME="py2" || FOLDER_NAME="py3"
+    if [ -f "package/lib/requirements.txt" ]; then
+        pip install -r package/lib/requirements.txt --user
+    fi
+    if [ -f "package/lib/${FOLDER_NAME}/requirements.txt" ]; then
+        pip install -r package/lib/${FOLDER_NAME}/requirements.txt --user
+    fi
+fi
+
+if [ "${IMAGE_TAG}" = "2.7.17" ]
+then
+    pip install -r requirements_py2_dev.txt
+    pip install six>=1.15
+else
+    pip install -r requirements_dev.txt --user
+    pip install six>=1.15 --user
+    pip install git+https://github.com/rfaircloth-splunk/agent-python-pytest.git --user
+fi
+
+cp -f .pytest.expect ${TEST_SET}
+
+echo "Executing Tests..."
+RERUN_COUNT=${RERUN_COUNT:-1}
+if [ -z ${TEST_BROWSER} ] 
+then
+    echo Test Args $@ --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+    -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+    ${TEST_SET}     
+    if [ "${IMAGE_TAG}" = "2.7.17" ]
+    then
+        pytest $@ \
+        ${TEST_SET} 
+        test_exit_code=$?
+    else
+        pytest $@ \
+        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+        ${TEST_SET}
+        test_exit_code=$?
+    fi
+else
+    # Execute the tests on Headless mode in local if UI_TEST_HEADLESS environment is set to "true"
+    if [ "${UI_TEST_HEADLESS}" = "true" ]
+    then
+        echo Test Args $@ --local --persist-browser --headless --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER}\
+        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+        ${TEST_SET} 
+        
+        pytest $@ --local --persist-browser --headless --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} \
+        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+        ${TEST_SET}
+        test_exit_code=$?
+    else
+        echo "Check Saucelab connection..."
+        wget --retry-connrefused --no-check-certificate -T 10 sauceconnect:4445
+        sauce_connect_connection=$?
+        echo "Sauce Connect Status:$sauce_connect_connection"
+        [ "$sauce_connect_connection" -eq "4" ] && echo "SauceConnect is not running. Exiting the tests...." && exit 1
+        echo Test Args $@ --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} ${TEST_SET}
+        pytest $@ --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} \
+        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+        ${TEST_SET}
+        test_exit_code=$?
+    fi
+fi
+exit "$test_exit_code" 
