669c302694a4f1603c7f425bfedb40dead53380c
srv-rr-github-token <94607705+srv-rr-github-token@users.noreply.github.com>
2022-08-16 12:41:31 +0200

    ci: bump repository configuration from template-refs/heads/test/pre-commit-linter (#185)
    * ci: common template rollout changes
    
    * style: pre-commit run --all-files

--- tests/knowledge/conftest.py
+++ tests/knowledge/conftest.py
@@ -5,13 +5,13 @@
 #
 import os
 import pytest
+
 pytest_plugins = "pytester"
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "external: Test search time only")
     config.addinivalue_line("markers", "docker: Test search time only")
-
 
 
 @pytest.fixture(scope="session")
@@ -27,6 +27,6 @@ def docker_compose_files(request):
     docker_compose_path = os.path.join(
         str(request.config.invocation_dir), "docker-compose.yml"
     )
-    #LOGGER.info("docker-compose path: %s", docker_compose_path)
+    # LOGGER.info("docker-compose path: %s", docker_compose_path)
 
     return [docker_compose_path]
